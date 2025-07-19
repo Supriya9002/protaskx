a6d52e122c8af5eda34de804cd328bac29313219
SupriyaPeerHub <supriyahaldar@peerhub.in>
2025-07-20 02:13:01 +0530

    set up file and folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UploadsModule } from './uploads/uploads.module';
+import { RedisService } from './cache/redis/redis.service';
+import { CacheModule } from './cache/cache.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, TasksModule, UploadsModule, CacheModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RedisService],
 })
 export class AppModule {}
